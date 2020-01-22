a45876c3b7f5cb45c60031cf917423cb035ae614
seezamGH <seezam@yandex.ru>
2020-01-22 20:58:52 +0300

    add cart

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-// тут я буду писать свой говнокод!
-console.log('тут я буду писать свой говнокод!')
+/**
+ * Корзина
+ */
+document.addEventListener('DOMContentLoaded', ()=>{
+
+  
+  const cart = document.querySelector('.cart-wrapper')
+  const cartOpenBtn = document.querySelector('.cart-open-btn')
+  const cartCloseBtn = document.querySelector('.cart-close-btn')
+  cartOpenBtn.addEventListener('click', ()=>{
+    cart.classList.add('cart-wrapper-open')
+  })
+  cartCloseBtn.addEventListener('click', ()=>{
+    cart.classList.remove('cart-wrapper-open')
+  })
+
+})
+
